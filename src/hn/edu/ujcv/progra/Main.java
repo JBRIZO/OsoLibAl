aa5dcecc6180c635c94633a30c6cbea1db49a4f7
JBRIZO <jonathan.laux@hotmail.com>
2020-03-09 08:18:42 -0600

    Transpuesta y toString para OMat3x3

--- src/hn/edu/ujcv/progra/Main.java
+++ src/hn/edu/ujcv/progra/Main.java
@@ -5,16 +5,8 @@ public class Main {
 
     public static void main(String[] args)
     {
-        /*OMat2x2 a = new OMat2x2(1,2,3,4);
-        OMat2x2 b = new OMat2x2(1,1,1,1);
-        OVecR2 a1 = new OVecR2(1,2);
-        OVecR2 a2 = new OVecR2(1,2);
-        System.out.println(a1.suma(a2));
-        System.out.println(a.rotacion(30));*/
-        OVecR3 a = new OVecR3(4,-2,1);
-        OVecR3 b = new OVecR3(3,-5,2);
-        System.out.println(a.magnitud());
-        System.out.println(a.suma(b));
-
+       OMat3x3 a = new OMat3x3(1,2,3,4,5,6,7,8,9);
+       OMat2x2 b = new OMat2x2(1,1,1,1);
+        System.out.println(a.transpuesta());
     }
 }
