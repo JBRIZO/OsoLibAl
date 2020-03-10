bb41168c46c1a1c59cb411dd2eecee6ee60be72c
JBRIZO <jonathan.laux@hotmail.com>
2020-03-10 08:18:15 -0600

    Sets, gets, constructores, suma y resta para OVecR4

--- src/hn/edu/ujcv/progra/OVecR4.java
+++ src/hn/edu/ujcv/progra/OVecR4.java
@@ -2,24 +2,65 @@ package hn.edu.ujcv.progra;
 
 public class OVecR4 {
     // miembros
-    private double x1;
-    private double x2;
-    private double x3;
-    private double x4;
+    private double x;
+    private double y;
+    private double z;
+    private double t;
 
+    // constructores
+    public OVecR4(){}
+
+    public OVecR4(OVecR4 a){}
+
+    public OVecR4(double x, double y, double z, double t)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.t = t;
+    }
 
     // accesoras y mutadoras
+    public double getX()
+    {
+        return x;
+    }
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
+    public double getZ()
+    {
+        return z;
+    }
+    public void setZ(double z)
+    {
+        this.z = z;
+    }
+
+    public double getT() { return t; }
+    public void setT(double t) { this.t = t; }
 
     // metodos
 
-    public OVecR4 suma(OVecR4 b){
-        //TODO: implementar
-        return new OVecR4();
+    public OVecR4 suma(OVecR4 b)
+    {
+        return new OVecR4(this.x + b.x, this.y + b.y, this.z + b.z, this.t + b.t);
     }
 
-    public OVecR4 resta(OVecR4 b){
-        //TODO: implementar
-        return new OVecR4();
+    public OVecR4 resta(OVecR4 b)
+    {
+        return new OVecR4(this.x - b.x, this.y - b.y, this.z - b.z, this.t - b.t);
     }
 
     public double prodPunto(OVecR4 b){
